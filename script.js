1a01d8800331b4cc83804311ffecc233925e8b55
alumno <alumno@Instituto.local>
2025-05-21 08:21:18 -0300

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/*ejercicio 1*/
+
+let frutas = ["frutilla", "banana", "mandarina"]
+
+console.log (frutas)
+
+
+frutas.push ("manzana");
+console.log (frutas[frutas.length-1]);
+
+frutas.unshift ("pera");
+console.log (frutas[0]);
+
+frutas.pop("manzana")
+console.log (frutas)
+
+/*ejercicio 2*/
+
